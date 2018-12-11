9d50f0d7948dc3faea7db3c2d820bb40b0751d7b
rbrown3 <rosannabrown@outlook.com>
2018-12-11 12:12:58 -0500

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,10 +3,17 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character_name)
-  #make the web request
+  films = []
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-
+  person_hash = response_hash["results"]
+   person_hash.each do |item|
+     if item["name"] == character_name
+      films << item["films"]
+    end
+  end
+  return films
+end
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,18 +23,25 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-end
+
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  list_films = []
+  films.each do |item|
+    item["title"]
+     list_films << item["title"]
+     end
+     list_films
+     binding.pry
 end
+
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
 
-## BONUS
+
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
